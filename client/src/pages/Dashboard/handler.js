17b59b99d7a2d8eefc67a44c644c4078f9e48f17
Neh Joshi <nehjoshi%5@gmail.com>
2022-09-22 23:51:40 +0530

    Improved token storage

--- client/src/pages/Dashboard/handler.js
+++ client/src/pages/Dashboard/handler.js
@@ -3,5 +3,5 @@ import { requestHeaderConfig } from "../../utils/requestHeader";
 import axios from 'axios';
 
 export const getTruthsAndLies = async () => {
-    return await axios.get(ENDPOINTS.GET_TRUTHS_AND_LIES, requestHeaderConfig);
+    return await axios.get(ENDPOINTS.GET_TRUTHS_AND_LIES, requestHeaderConfig(sessionStorage.getItem('token')));
 }
--- client/src/utils/requestHeader.js
+++ client/src/utils/requestHeader.js
@@ -1,8 +1,10 @@
-export const requestHeaderConfig = {
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        // 'Authorization': 'Bearer ' + document.cookie.split('=')[1]
-        'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+export const requestHeaderConfig = (token) => {
+    return {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            // 'Authorization': 'Bearer ' + document.cookie.split('=')[1]
+            'Authorization': 'Bearer ' + token
+        }
     }
-};
+}
